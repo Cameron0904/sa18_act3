76ff361092de1da946735cf3e4d2bfa554271c65
Cameron Cox <cameronacox0904@gmail.com>
2024-04-22 21:20:42 -0500

    Initial commit

---
+++ sa18_act3/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sa18_act3 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sa18_act3.settings')
+
+application = get_wsgi_application()
